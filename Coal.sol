3b091e937ccc9e6304e6a54a98d6329b0c8333ad
Tully-cmd <tullymartin@u.boisestate.edu>
2021-03-13 13:59:08 -0700

    a start

--- Coal.sol
+++ Coal.sol
@@ -1,3 +1,46 @@
-//TODO
+// SPDX-License-Identifier: GPL-3.0
 
-test commit
+pragma solidity >=0.7.0 <0.8.0;
+
+/** 
+ * @title Coal
+ * @dev Idea on how a emergency power system could work
+ */
+contract Coal {
+
+    //bool hasPower;
+    uint currentHousePower;
+    uint minimumLevel; // min level this house needs to have power
+    uint extraGridPower = 1000; //this somehow needs to be a value that is always
+                                //the same for all users on this contract
+                                //lets say there is 1000 extra power to start out with
+    
+    //user passes in if they have power or not when starting up
+    constructor (uint currentPower, uint min) public { 
+        //hasPower = status;
+        currentHousePower = currentPower;
+        minimumLevel = min;
+    }
+    
+    function sendExtraCoal(uint amount) payable public {
+        if(minimumLevel > currentHousePower - amount ) {
+            //FAIL GAVE TOO MUCH POWER
+        } else {
+            extraGridPower = extraGridPower + amount;
+            currentHousePower = currentHousePower - amount;
+        }
+    }
+    
+    function getEmergencyPower() public {
+        if(minimumLevel > currentHousePower) {
+            //able to recieve 
+            uint neededAmount = minimumLevel - currentHousePower;
+            extraGridPower = extraGridPower - neededAmount;
+            currentHousePower = currentHousePower + neededAmount;
+        } else {
+            //fail already have enough power!
+        }
+    }
+    
+
+}
